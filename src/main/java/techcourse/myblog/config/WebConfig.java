f2c74f4cd8edf5d44d187a8e9371b9e6955820b5
pkch93 <star_final@naver.com>
2019-08-01 15:37:02 +0900

    feat: AuthorizedUserArgumentResolver class
    - Session에 저장된 User 정보를 Argument에 매핑해주는 ArgumentResolver 구현
    - Spring에 등록 (WebConfig)

--- src/main/java/techcourse/myblog/config/WebConfig.java
+++ src/main/java/techcourse/myblog/config/WebConfig.java
@@ -2,8 +2,14 @@ package techcourse.myblog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.myblog.config.argumentresolver.AuthorizedUserArgumentResolver;
+import techcourse.myblog.config.interceptor.NeedAuthInterceptor;
+import techcourse.myblog.config.interceptor.NoneAuthInterceptor;
+
+import java.util.List;
 
 @Configuration
 @EnableJpaAuditing
@@ -20,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(new NeedAuthInterceptor())
                 .addPathPatterns("/auth/login", "/users/signup");
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthorizedUserArgumentResolver());
     }
 }
